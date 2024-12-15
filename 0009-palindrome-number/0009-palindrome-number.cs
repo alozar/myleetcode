6585103cf0261827f8c83d6e4d334f91163aab3a
Александр Зарков <alozar@users.noreply.github.com>
2024-12-15 23:16:00 +0800

    Time: 1 ms (99.84%), Space: 32.5 MB (68.82%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cs
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -1,29 +1,18 @@
 public class Solution {
-    public bool IsPalindrome(int x) {
-        //var xString = x.ToString();
-        //var xReverseString = new string(xString.Reverse().ToArray());
-        //return xString.Equals(xReverseString);
-
-        if (x < 0)
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0 || (x % 10 == 0 && x != 0))
         {
             return false;
-        }  
+        }
 
-        List<int> reverseDigits = new ();
-        while (x != 0)
+        int revertedHalfNumber = 0;
+        while (x > revertedHalfNumber)
         {
-            reverseDigits.Add(x % 10);
+            revertedHalfNumber = revertedHalfNumber * 10 + x % 10;
             x /= 10;
         }
 
-        for (var i = 0; i < reverseDigits.Count / 2; i++)
-        {
-            if (reverseDigits[i] != reverseDigits[reverseDigits.Count - 1 - i])
-            {
-                return false;
-            }
-        }
-
-        return true;
+        return x == revertedHalfNumber || x == revertedHalfNumber / 10;
     }
 }
